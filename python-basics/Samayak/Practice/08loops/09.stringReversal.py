209f83c558b0f6cc839feca5afb053e88f583514
Harsh Boyal <123317775+Erupter20@users.noreply.github.com>
2026-03-09 09:12:00 +0530

    Update 09.stringReversal.py

--- python-basics/Samayak/Practice/08loops/09.stringReversal.py
+++ python-basics/Samayak/Practice/08loops/09.stringReversal.py
@@ -1,5 +1,6 @@
 # 09. String Reversal: Write a program that takes a string and prints it in reverse order using a loop.
 
-str = "hello world"
-for char in str:
-    print(char) # how to reverse?
+s = "hello world"
+
+for i in range(len(s)-1, -1, -1):
+    print(s[i])
