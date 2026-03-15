8f87168dede12b27573620c93b2de402589cb43d
Erupter20 <boyalharsh1@gmail.com>
2026-03-15 15:08:14 +0530

    fix: disable flask reloader for streamlit cloud

--- python-basics/Samayak/Projects/attendance-tracker/app.py
+++ python-basics/Samayak/Projects/attendance-tracker/app.py
@@ -391,6 +391,7 @@ def upload_file():
 # ---------------- START SERVER ----------------
 
 if __name__ == "__main__":
-
     init_db()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False, use_reloader=False)
+    
